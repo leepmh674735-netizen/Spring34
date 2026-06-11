7f34d48e51a7acffe3f5b59c73e2beac279961b2
Yubin <leepmh674735@gmail.com>
2026-06-11 18:30:20 +0900

    12

--- src/main/java/com/example/demo/repository/AuthorityRepository.java
+++ src/main/java/com/example/demo/repository/AuthorityRepository.java
@@ -1,5 +1,12 @@
 package com.example.demo.repository;
 
-public interface AuthorityRepository {
+import java.lang.reflect.Member;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Authority;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long>{
+    List<Authority> findByMember(Member mmeber);
 }
--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repository;
 
-public interface MemberRepository {
+import java.lang.reflect.Member;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long>{
+   Optional<Member> findByEmail(String email);
 }
